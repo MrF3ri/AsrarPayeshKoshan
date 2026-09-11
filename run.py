74029c11eee402d6f76d8c40676d2c0f3252e013
Mr F3ri <109946641+MrF3ri@users.noreply.github.com>
2026-09-11 13:34:08 +0330

    update

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
-# from fakeData import main  <-- این را کامنت کنید
 
 app = create_app()
 
 if __name__ == "__main__":
-    # main()  <-- این را هم کامنت کنید
     app.run(host="0.0.0.0", port=5000, debug=True)
